cd13cb91d475450c2cf191c9e2b91a0ffe0b2ec5
Mark Hording <markhording@gmail.com>
2025-06-30 12:00:48 +0700

    Refactor JavaScript: Implement DRY principle with reusable toggleAccordion function

--- script.js
+++ script.js
@@ -1,47 +1,37 @@
-document.querySelector('.icon-faq').addEventListener('click', function(e) {
-  document.querySelector('.main-text').style.display = 'block';
-  document.querySelector('.icon-minus').style.display = 'block';
-  document.querySelector('.icon-faq').style.display = 'none';
-});
+function toggleAccordion(plusIcon, minusIcon, text) {
+  plusIcon.addEventListener('click', function() {
+    text.style.display = 'block';
+    minusIcon.style.display = 'block';
+    plusIcon.style.display = 'none';
+  });
 
-document.querySelector('.icon-minus').addEventListener('click', function(e) {
-  document.querySelector('.main-text').style.display = 'none';
-  document.querySelector('.icon-minus').style.display = 'none';
-  document.querySelector('.icon-faq').style.display = 'block';
-});
+  minusIcon.addEventListener('click', function() {
+    text.style.display = 'none';
+    minusIcon.style.display = 'none';
+    plusIcon.style.display = 'block';
+  });
+}
 
-document.querySelector('.icon-plus-free').addEventListener('click', function(e) {
-  document.querySelector('.free-text').style.display = 'block';
-  document.querySelector('.icon-minus-free').style.display = 'block';
-  document.querySelector('.icon-plus-free').style.display = 'none';
-});
+toggleAccordion(
+  document.querySelector('.icon-faq'),
+  document.querySelector('.icon-minus'),
+  document.querySelector('.main-text')
+);
 
-document.querySelector('.icon-minus-free').addEventListener('click', function(e) {
-  document.querySelector('.free-text').style.display = 'none';
-  document.querySelector('.icon-minus-free').style.display = 'none';
-  document.querySelector('.icon-plus-free').style.display = 'block';
-});
+toggleAccordion(
+  document.querySelector('.icon-plus-free'),
+  document.querySelector('.icon-minus-free'),
+  document.querySelector('.free-text')
+);
 
-document.querySelector('.icon-plus-mentor').addEventListener('click', function(e) {
-  document.querySelector('.mentor-text').style.display = 'block';
-  document.querySelector('.icon-minus-mentor').style.display = 'block';
-  document.querySelector('.icon-plus-mentor').style.display = 'none';
-});
+toggleAccordion(
+  document.querySelector('.icon-plus-mentor'),
+  document.querySelector('.icon-minus-mentor'),
+  document.querySelector('.mentor-text')
+);
 
-document.querySelector('.icon-minus-mentor').addEventListener('click', function(e) {
-  document.querySelector('.mentor-text').style.display = 'none';
-  document.querySelector('.icon-minus-mentor').style.display = 'none';
-  document.querySelector('.icon-plus-mentor').style.display = 'block';
-});
-
-document.querySelector('.icon-plus-challenge').addEventListener('click', function(e) {
-  document.querySelector('.challenge-text').style.display = 'block';
-  document.querySelector('.icon-minus-challenge').style.display = 'block';
-  document.querySelector('.icon-plus-challenge').style.display = 'none';
-});
-
-document.querySelector('.icon-minus-challenge').addEventListener('click', function(e) {
-  document.querySelector('.challenge-text').style.display = 'none';
-  document.querySelector('.icon-minus-challenge').style.display = 'none';
-  document.querySelector('.icon-plus-challenge').style.display = 'block';
-});
+toggleAccordion(
+  document.querySelector('.icon-plus-challenge'),
+  document.querySelector('.icon-minus-challenge'),
+  document.querySelector('.challenge-text')
+);
